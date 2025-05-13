a581e4caba307c67d6f439e58258f8bf80ae68ea
Wencan Peng <p.wencan@outlook.com>
2025-05-13 14:39:29 +1000

    Reinitialized db and adapted local db instance

--- gateway/app_factory.py
+++ gateway/app_factory.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from database.models import db
 from flask_migrate import Migrate
+import os
 
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app/instance/imagepipe.db'
+
+    # database config compatible local dev adaptation
+    db_uri = os.environ.get("IMAGEPIPE_DB_URI")
+    if db_uri:
+        app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/imagepipe.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     migrate.init_app(app, db)
