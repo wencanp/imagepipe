02f92b5ad82c3f8dde58514c0c99e6de3c94b7d5
Wencan Peng <p.wencan@outlook.com>
2025-05-27 15:58:52 +1000

    Added test cat

--- imagepipe-ui/entrypoint.sh
+++ imagepipe-ui/entrypoint.sh
@@ -3,5 +3,8 @@
 # Replace env var, generate nginx.conf
 envsubst '$GATEWAY_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
+echo "----- FINAL nginx.conf -----"
+cat /etc/nginx/nginx.conf
+
 # cmd launch Nginx
 nginx -g 'daemon off;'
