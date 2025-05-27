dc032f06f134dbae58e66dbe12c90edd9e096bd0
Wencan Peng <p.wencan@outlook.com>
2025-05-27 14:32:28 +1000

    Corrected filename

--- imagepipe-ui/entrypoint.sh
+++ imagepipe-ui/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace env var, generate nginx.conf
-envsubst '$GATEWAY_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$GATEWAY_URL' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
 
 # cmd launch Nginx
 nginx -g 'daemon off;'
