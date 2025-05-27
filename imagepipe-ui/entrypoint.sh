b46162f3116d09110f89ddaa568cc4bdf50cf2eb
Wencan Peng <p.wencan@outlook.com>
2025-05-27 15:00:54 +1000

    Fixed spelling

--- imagepipe-ui/entrypoint.sh
+++ imagepipe-ui/entrypoint.sh
@@ -3,8 +3,5 @@
 # Replace env var, generate nginx.conf
 envsubst '$GATEWAY_URL' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
 
-# test nginx.conf
-cat /etc/nginx/nginx.conf
-
 # cmd launch Nginx
 nginx -g 'daemon off;'
