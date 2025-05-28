7d9948a5fe5056c6ab3ae9b42e56ad8227a79ef8
Wencan Peng <p.wencan@outlook.com>
2025-05-29 03:08:36 +1000

    gateway: modify expire log & debug log

--- gateway/support.py
+++ gateway/support.py
@@ -1,23 +1,24 @@
 import time
+from datetime import datetime, timedelta
 
 from urllib.parse import urlparse, parse_qs
 
 def is_minio_url_expired(url):
     """ Check if a MinIO presigned URL has expired."""
     try:
+        print("enter is_minio_url_expired")
         parsed = urlparse(url)
         query = parse_qs(parsed.query)
+        print("check query")
         expires_in = int(query.get('X-Amz-Expires', [0])[0])
         signed_time_str = query.get('X-Amz-Date', [None])[0]
-
+        print("check signed_time_str")
         if not signed_time_str:
             return True  # cannot determine expiry, assume expired
-
-        from datetime import datetime, timedelta
+        print("check expires time")
         signed_time = datetime.strptime(signed_time_str, "%Y%m%dT%H%M%SZ")
         expiry_time = signed_time + timedelta(seconds=expires_in)
 
-        print(f"signed_time: {signed_time}, expiry_time: {expiry_time}")
-        return time.time() < expiry_time.timestamp()
+        return time.time() > expiry_time.timestamp()
     except Exception as e:
         return True  # if any error occurs, assume expired
